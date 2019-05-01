0671e914bf14a0c63917194cf8cdae7e92d826d8
huazai <who.seek.me@java98k.vip>
2019-05-01 08:53:19 +0800

    初始化项目

---
+++ src/main/java/com/huazai/plus/controller/MybatisPlusGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.huazai.plus.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusGeneratorApplication.class, args);
+	}
+
+}
