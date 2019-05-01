718df10a246dd01629554f74e2689f83f5dbc4a1
huazai <who.seek.me@java98k.vip>
2019-05-01 15:13:03 +0800

    新增MyBatisConfig配置类

---
+++ src/main/java/com/huazai/plus/common/CString.java
@@ -0,0 +1,115 @@
+package com.huazai.plus.common;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+
+/**
+ * 
+ * <p>
+ * 
+ * @ClassName : CBoolean
+ *            </p>
+ *            <p>
+ * @Description : TODO
+ *              </p>
+ * 
+ * @Author : HuaZai
+ * @CreateDate : 2019年4月29日 下午2:45:05
+ * @ContactInformation : 1461522031@qq.com/huazai6789@aliyun.com
+ *
+ * @PackgeName : com.huazai.plus.common.CBoolean
+ *
+ * @Version : V1.0.0
+ */
+public interface CString {
+
+	// ---------------------------------------------------------------- file suffix
+	String C_DOT_CLASS = ".class";
+	String C_DOT_JAVA = ".java";
+	String C_DOT_XML = ".xml";
+	String C_DOT_JSP = ".jsp";
+	String C_DOT_HTML = ".html";
+
+	// ---------------------------------------------------------------- boolean
+	String C_FALSE = "false";
+	String C_TRUE = "true";
+	boolean C_TRUE_P = true;
+	boolean C_FALSE_P = false;
+
+	// ---------------------------------------------------------------- encoding
+	String C_UTF_8 = "UTF-8";
+	String C_GBK = "GBK";
+	String C_GB2312 = "GB2312";
+	String C_ASCII = "ASCII";
+	String C_US_ASCII = "US-ASCII";
+	String C_ISO_8859_1 = "ISO-8859-1";
+
+	// ---------------------------------------------------------------- symbol
+	String C_HTML_NBSP = "&nbsp;";
+	String C_HTML_AMP = "&amp";
+	String C_HTML_QUOTE = "&quot;";
+	String C_HTML_LT = "&lt;";
+	String C_HTML_GT = "&gt;";
+
+	// ---------------------------------------------------------------- all
+	String C_AMPERSAND = "&";
+	String C_AND = "and";
+	String C_AT = "@";
+	String C_BACK_SLASH = "\\";
+	String C_COLON = ":";
+	String C_COMMA = ",";
+	String C_DOLLAR = "$";
+	String C_DOT = ".";
+	String C_DOTDOT = "..";
+	String C_EMPTY = "";
+	String C_EQUALS = "=";
+	String C_PLUS = "+";
+	String C_DASH = "-";
+	String C_ASTERISK = "*";
+	String C_STAR = C_ASTERISK;
+	String C_SLASH = "/";
+	String C_HASH = "#";
+	String C_HAT = "^";
+	String C_LEFT_BRACE = "{";
+	String C_LEFT_BRACKET = "(";
+	String C_LEFT_CHEV = "<";
+	String C_DOT_NEWLINE = ",\n";
+	String C_NEWLINE = "\n";
+	String C_PERCENT = "%";
+	String C_PIPE = "|";
+	String C_QUESTION_MARK = "?";
+	String C_EXCLAMATION_MARK = "!";
+	String C_QUOTE = "\"";
+	String C_RETURN = "\r";
+	String C_TAB = "\t";
+	String C_RIGHT_BRACE = "}";
+	String C_RIGHT_BRACKET = ")";
+	String C_RIGHT_CHEV = ">";
+	String C_SEMICOLON = ";";
+	String C_SINGLE_QUOTE = "'";
+	String C_BACKTICK = "`";
+	String C_SPACE = " ";
+	String C_TILDA = "~";
+	String C_LEFT_SQ_BRACKET = "[";
+	String C_RIGHT_SQ_BRACKET = "]";
+	String C_UNDERSCORE = "_";
+	String C_N = "n";
+	String C_Y = "y";
+	String C_NO = "no";
+	String C_YES = "yes";
+	String C_NULL = "null";
+	String C_OFF = "off";
+	String C_ON = "on";
+	String C_ONE = "1";
+	String C_ZERO = "0";
+	String C_TWO = "2";
+	String C_THREE = "3";
+	String C_DOLLAR_LEFT_BRACE = "${";
+	String C_HASH_LEFT_BRACE = "#{";
+	String C_CRLF = "\r\n";
+
+	// ---------------------------------------------------------------- array
+	String[] EMPTY_ARRAY = new String[0];
+
+	byte[] BYTES_NEW_LINE = StringPool.NEWLINE.getBytes();
+
+}
---
+++ src/main/java/com/huazai/plus/config/MyBatisConfig.java
@@ -0,0 +1,184 @@
+package com.huazai.plus.config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.FileOutConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.IFileCreate;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.FileType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.huazai.plus.common.CString;
+import com.huazai.plus.common.Constant;
+
+public class MyBatisConfig {
+
+	public static void generation() {
+		
+		// 初始化代码生成器
+		final AutoGenerator generator = new AutoGenerator();
+
+		// 全局参数配置
+		final GlobalConfig gConfig = new GlobalConfig();
+		gConfig.setAuthor(Constant.GLOBAL_AUTHOR);
+		// 从系统参数中获取
+		// String projectPath = System.getProperty("user.dir");
+		// gConfig.setOutputDir(projectPath + "/src/main/java");
+		gConfig.setOutputDir(Constant.GLOBAL_OUTDIR);
+		// 是否覆盖同名文件，默认是false
+		gConfig.setFileOverride(CString.C_TRUE_P);
+		// 不需要ActiveRecord特性的请改为false
+		gConfig.setActiveRecord(CString.C_TRUE_P);
+		// XML 二级缓存
+		gConfig.setEnableCache(CString.C_FALSE_P);
+		// XML ResultMap
+		gConfig.setBaseResultMap(CString.C_TRUE_P);
+		// XML columList
+		gConfig.setBaseColumnList(CString.C_TRUE_P);
+		// 自定义文件命名，注意 %s 会自动填充表实体属性
+		gConfig.setMapperName(Constant.GLOBAL_MAPPERNAME);
+		gConfig.setXmlName(Constant.GLOBAL_XMLNAME);
+		gConfig.setServiceName(Constant.GLOBAL_SERVICENAME);
+		gConfig.setServiceImplName(Constant.GLOBAL_SERVICEIMPLNAME);
+		gConfig.setControllerName(Constant.GLOBAL_CONTROLLERNAME);
+		// true 生成后 open explorer;false 不打开
+		// gConfig.setOpen(false);
+		generator.setGlobalConfig(gConfig);
+
+		// 数据源配置
+		final DataSourceConfig dbConfig = new DataSourceConfig();
+		// 数据库类型
+		dbConfig.setDbType(DbType.MYSQL);
+		// 数据库驱动
+		// 注意：MySQL5驱动为：com.mysql.jdbc.Driver；MySQL6驱动为：com.mysql.cj.jdbc.Driver
+		dbConfig.setDriverName(Constant.MYSQL_DRIVER_NAME);
+		// 数据库地址
+		dbConfig.setUrl(Constant.MYSQL_URI);
+		dbConfig.setUsername(Constant.MYSQL_USERNAME);
+		dbConfig.setPassword(Constant.MYSQL_PASSWORD);
+		generator.setDataSource(dbConfig);
+
+		// 包配置
+		final PackageConfig pConfig = new PackageConfig();
+		// pConfig.setModuleName("模块名");
+		pConfig.setParent(Constant.PACKAGE_PARENT);
+		pConfig.setEntity(Constant.PACKAGE_ENTITY);
+		pConfig.setMapper(Constant.PACKAGE_MAPPER);
+		pConfig.setService(Constant.PACKAGE_SERVICE);
+		pConfig.setServiceImpl(Constant.PACKAGE_SERVICEIMPL);
+		pConfig.setController(Constant.PACKAGE_CONTROLLER);
+		generator.setPackageInfo(pConfig);
+
+		/**
+		 * 自定义配置
+		 */
+		final InjectionConfig iConfig = new InjectionConfig() {
+
+			@Override
+			public void initMap() {
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("aiyou", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+				this.setMap(map);
+			}
+		};
+		final List<FileOutConfig> fConfigs = new ArrayList<>();
+		fConfigs.add(new FileOutConfig(Constant.TEMPLATE_FREEMARKER_PATH) {
+
+			@Override
+			public String outputFile(TableInfo tableInfo) {
+
+				// 自定义输出文件名 ， 如果 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化
+				return pConfig.getParent() + Constant.OUTFILE_XMLBASEURL + pConfig.getModuleName() + StringPool.SLASH
+						+ tableInfo.getEntityName() + Constant.OUTFILE_XMLSUFFIXNAME + StringPool.DOT_XML;
+			}
+		});
+		iConfig.setFileCreate(new IFileCreate() {
+			@Override
+			public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+
+				// true 创建目录和内容，false 只创建目录
+				// return false;
+				return true;
+			}
+		});
+		iConfig.setFileOutConfigList(fConfigs);
+		generator.setCfg(iConfig);
+
+		/**
+		 * 配置模板：
+		 * <li>自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
+		 * <li>放置到自己项目的 src/main/resources/templates 目录下,
+		 * <li>默认名称可以不配置，也可以根据自己实际的业务需求自定义模板名称
+		 */
+		TemplateConfig tConfig = new TemplateConfig();
+		// 配置自定义输出模板，指定自定义模板路径， 注意不要带上.ftl/.vm, 如果为null，则表示不生成文件，如果不指定模板，则会根据使用的模板引擎自动识别
+		// tConfig.setEntity(null);
+		// tConfig.setXml(null);
+		// tConfig.setMapper(null);
+		// tConfig.setService(null);
+		// tConfig.setServiceImpl(null);
+		// tConfig.setController(null);
+		generator.setTemplate(tConfig);
+
+		/**
+		 * 策略配置
+		 */
+		StrategyConfig sConfig = new StrategyConfig();
+		// 表名生成策略
+		sConfig.setNaming(NamingStrategy.underline_to_camel);
+		// 列生成策略
+		sConfig.setColumnNaming(NamingStrategy.underline_to_camel);
+		// 自定义实体父类
+		// sConfig.setSuperEntityClass(Constant.Strategy_SuperEntity);
+		// 自定义公共字段
+		sConfig.setSuperEntityColumns("id");
+		// 实体是否使用 Lombok 模型
+		sConfig.setEntityLombokModel(CString.C_TRUE_P);
+		// 自定义 mapper 父类
+		// sConfig.setSuperMapperClass(Constant.STRATEGY_SUPERMAPPER);
+		// 自定义 service 父类
+		// sConfig.setSuperServiceClass(Constant.STRATEGY_SUPERSERVICE);
+		// 自定义 service 实现类父类
+		// sConfig.setSuperServiceImplClass(Constant.STRATEGY_SUPERSERVICEIMPL);
+		// 自定义 controller 父类
+		// sConfig.setSuperControllerClass(Constant.STRATEGY_SUPERCONTROLLER);
+		// controller 是否使用 rest 风格
+		sConfig.setRestControllerStyle(CString.C_TRUE_P);
+		// controller 的映射风格
+		sConfig.setControllerMappingHyphenStyle(CString.C_TRUE_P);
+		// 自定义表前缀
+		// sConfig.setTablePrefix(pConfig.getModuleName() + "_");
+		sConfig.setTablePrefix(Constant.STRATEGY_TABLEPREFIX);
+		// 需要生成的表(多个表之前使用英文逗号隔开)
+		sConfig.setInclude(Constant.STRATEGY_INCLUDE);
+		generator.setStrategy(sConfig);
+
+		/**
+		 * 配置模板引擎
+		 */
+		// final FreemarkerTemplateEngine fTemplateEngine = new
+		// FreemarkerTemplateEngine();
+		// generator.setTemplateEngine(fTemplateEngine);
+
+		/**
+		 * 执行
+		 */
+		generator.execute();
+
+		// 打印注入值
+		System.err.println(generator.getCfg().getMap().get("aiyou"));
+	}
+
+}
